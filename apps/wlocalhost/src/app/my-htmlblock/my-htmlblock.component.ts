96e636920987fc538e2c07bef38bb4fd4ce9ace5
wanoo21 <ionprodan33@gmail.com>
2022-06-29 17:44:06 +0300

    chore(app): Test layout

--- apps/wlocalhost/src/app/my-htmlblock/my-htmlblock.component.ts
+++ apps/wlocalhost/src/app/my-htmlblock/my-htmlblock.component.ts
@@ -1,18 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import {
-  AIPEmailBuilderBlock,
-  IPadding,
-} from '@wlocalhost/ngx-email-builder/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from "@angular/core";
+import { AIPEmailBuilderBlock, IPadding } from "@wlocalhost/ngx-email-builder/core";
 
 @Component({
-  selector: 'wlocalhost-my-htmlblock',
+  selector: "wlocalhost-my-htmlblock",
   template: `
     <p>my-htmlblock works! {{ innerText }}</p>
-    <ng-container *cdk-portal>
+    <ng-container *ipEmailBuilderSettings>
       <input type="text" [(ngModel)]="innerText" placeholder="Change me" />
     </ng-container>
   `,
@@ -21,16 +14,16 @@ import {
       :host {
         display: block;
       }
-    `,
+    `
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.ShadowDom,
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class MyHTMLBlockComponent extends AIPEmailBuilderBlock<{
   padding: IPadding;
 }> {
-  innerText = 'HTML';
+  innerText = "HTML";
   options = {
-    padding: { top: 10 },
+    padding: { top: 10 }
   };
 }
--- apps/wlocalhost/src/app/nx-welcome.component.ts
+++ apps/wlocalhost/src/app/nx-welcome.component.ts
@@ -1,22 +1,17 @@
-import { Component } from '@angular/core';
-import {
-  AIPEmailBuilderService,
-  IPEmail,
-  Structure,
-} from '@wlocalhost/ngx-email-builder/core';
-import { AbsPage } from '@ngcomma/ngx-abstract';
+import { Component } from "@angular/core";
+import { AIPEmailBuilderService, IPEmail, Structure } from "@wlocalhost/ngx-email-builder/core";
+import { AbsPage } from "@ngcomma/ngx-abstract";
 
 @Component({
-  selector: 'wlocalhost-nx-welcome',
-  template: ` <ip-email-builder></ip-email-builder> `,
-  styles: [],
+  selector: "wlocalhost-nx-welcome",
+  template: `
+    <ip-email-builder></ip-email-builder> `,
+  styles: []
 })
 export class NxWelcomeComponent extends AbsPage {
-  email = new IPEmail({
-    structures: [new Structure('cols_6')],
-  });
+  email = new IPEmail([new Structure("cols_6")]);
 
   constructor(readonly ngb: AIPEmailBuilderService) {
-    super('Home', 'Wlocalhost description', true);
+    super("Home", "Wlocalhost description", true);
   }
 }
