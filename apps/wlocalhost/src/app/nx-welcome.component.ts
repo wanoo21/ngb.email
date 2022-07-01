de633411da3e6b01b9aec71f71f0eea89130c495
wanoo21 <ionprodan33@gmail.com>
2022-07-01 17:59:50 +0300

    chore(app): Email object from demo

--- apps/wlocalhost/src/app/nx-welcome.component.ts
+++ apps/wlocalhost/src/app/nx-welcome.component.ts
@@ -1,15 +1,28 @@
 import { Component } from "@angular/core";
-import { AIPEmailBuilderService, IPEmail, Structure } from "@wlocalhost/ngx-email-builder/core";
+import {
+  AIPEmailBuilderService,
+  ImageBlockComponent,
+  IPEmail,
+  Structure,
+  TextBlockComponent
+} from "@wlocalhost/ngx-email-builder/core";
 import { AbsPage } from "@ngcomma/ngx-abstract";
 
 @Component({
   selector: "wlocalhost-nx-welcome",
   template: `
-    <ip-email-builder></ip-email-builder> `,
+    <ip-email-builder [email]="email"></ip-email-builder> `,
   styles: []
 })
 export class NxWelcomeComponent extends AbsPage {
-  email = new IPEmail([new Structure("cols_6")]);
+  email = new IPEmail([
+    new Structure("cols_2", [
+      [new TextBlockComponent(), new ImageBlockComponent()], [new TextBlockComponent()]
+    ]),
+    new Structure("cols_6", [
+      [new TextBlockComponent()]
+    ])
+  ]);
 
   constructor(readonly ngb: AIPEmailBuilderService) {
     super("Home", "Wlocalhost description", true);
