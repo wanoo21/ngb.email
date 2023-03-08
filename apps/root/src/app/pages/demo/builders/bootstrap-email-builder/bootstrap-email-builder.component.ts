f85d91d3152b7b7c924f0b9fb245c7e368bee80a
iceava <dovbenco@outlook.com>
2023-03-08 17:00:22 +0200

    style(bootstrap-email-builder): refactor-styles

--- apps/root/src/app/pages/demo/builders/bootstrap-email-builder/bootstrap-email-builder.component.ts
+++ apps/root/src/app/pages/demo/builders/bootstrap-email-builder/bootstrap-email-builder.component.ts
@@ -9,7 +9,8 @@ import { AbstractDemoEmailBuilder } from "../abstract-demo-email-builder";
   standalone: true,
   imports: [CommonModule, BootstrapEmailBuilderModule],
   template: `
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <bt-email-builder></bt-email-builder>`,
   styles: [
     `
