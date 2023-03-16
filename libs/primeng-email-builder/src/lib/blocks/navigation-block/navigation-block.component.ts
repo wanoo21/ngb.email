fd68677f31c5437c1f8d7ec69ec1d32901770521
iceava <dovbenco@outlook.com>
2023-03-16 17:46:12 +0200

    refactor(primeng-email-builder): add navigation-block

---
+++ libs/primeng-email-builder/src/lib/blocks/navigation-block/navigation-block.component.ts
@@ -0,0 +1,12 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {NavigationBlock} from "@wlocalhost/ngx-email-builder";
+
+@Component({
+  selector: 'prime-navigation-block',
+  templateUrl: './navigation-block.component.html',
+  styleUrls: ['./navigation-block.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class NavigationBlockComponent extends NavigationBlock{
+}
