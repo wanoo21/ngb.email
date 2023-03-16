b6e2be4a4536ee96d2c4b952891ec56cf5313a93
wanoo21 <ionprodan33@gmail.com>
2023-03-16 10:10:30 +0200

    refactor(tailwind-email-builder): preview devices

--- libs/tailwind-email-builder/src/lib/ip-email-builder/tail-email-builder.component.ts
+++ libs/tailwind-email-builder/src/lib/ip-email-builder/tail-email-builder.component.ts
@@ -1,31 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { AIPEmailBuilderComponent } from '@wlocalhost/ngx-email-builder';
-
-export type Device = 'desktop' | 'tablet' | 'mobile';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { AIPEmailBuilderComponent } from "@wlocalhost/ngx-email-builder";
 
 @Component({
-  selector: 'tail-email-builder',
-  templateUrl: './tail-email-builder.component.html',
-  styleUrls: ['./tail-email-builder.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "tail-email-builder",
+  templateUrl: "./tail-email-builder.component.html",
+  styleUrls: ["./tail-email-builder.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TailEmailBuilderComponent extends AIPEmailBuilderComponent {
-  widthDisplay = '100%';
-  device: Device = 'desktop';
-
-  changeDevice(device: Device): void {
-    switch (device) {
-      case "tablet":
-        this.widthDisplay = '768px';
-        this.device = 'tablet';
-      break;
-      case "mobile":
-        this.widthDisplay = '360px';
-        this.device = 'mobile';
-      break;
-      default:
-        this.widthDisplay = '100%';
-        this.device = 'desktop';
-    }
-  }
 }
