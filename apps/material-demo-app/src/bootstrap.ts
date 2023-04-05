0a421a3e649a30b67cbc7929af6a64f36293af5a
wanoo21 <ionprodan33@gmail.com>
2023-04-05 11:32:53 +0300

    fix(material-demo-app): add material design to application

--- apps/material-demo-app/src/bootstrap.ts
+++ apps/material-demo-app/src/bootstrap.ts
@@ -1,10 +1,7 @@
-import { ViewEncapsulation } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    defaultEncapsulation: ViewEncapsulation.Emulated,
-  })
+  .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
