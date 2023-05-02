73605ca774f002109d6b5094fa55fb435bf7788b
wanoo21 <ionprodan33@gmail.com>
2023-05-02 12:14:52 +0300

    refactor(root): first page

--- apps/root/src/app/pages/index/v14-info/v14-info.component.ts
+++ apps/root/src/app/pages/index/v14-info/v14-info.component.ts
@@ -1,9 +1,5 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatExpansionModule } from "@angular/material/expansion";
 import { FormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: "wlocalhost-v14-info",
@@ -11,11 +7,7 @@ import { MatInputModule } from "@angular/material/input";
   styleUrls: ["./v14-info.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    MatCardModule,
-    MatButtonModule,
-    MatExpansionModule,
-    FormsModule,
-    MatInputModule
+    FormsModule
   ],
   standalone: true
 })
