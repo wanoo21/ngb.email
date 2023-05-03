920570f2cd4c2d6a005f25004f9053baeab643cf
wanoo21 <ionprodan33@gmail.com>
2023-05-03 11:21:08 +0300

    refactor(root): first page

--- apps/root/src/app/pages/index/index.component.ts
+++ apps/root/src/app/pages/index/index.component.ts
@@ -21,6 +21,6 @@ import { V14InfoComponent } from "./v14-info/v14-info.component";
 })
 export class IndexComponent extends AbstractPage {
   constructor() {
-    super("MJML and Angular WYSIWYG Email Template Editor", "A developer-first email template builder based on Angular and MJML.", false);
+    super("Developer-First Email Template Builder by NGB", "A developer-first email template builder based on Angular and MJML.", false);
   }
 }
