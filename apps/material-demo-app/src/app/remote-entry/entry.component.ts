0b5116bbf1e8fbd5317265befb9cbd27dd0c7a0d
wanoo21 <ionprodan33@gmail.com>
2023-05-11 11:11:35 +0300

    feat(material-demo-app): add some styles and a new default structure

--- apps/material-demo-app/src/app/remote-entry/entry.component.ts
+++ apps/material-demo-app/src/app/remote-entry/entry.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { MaterialEmailBuilderModule } from "@wlocalhost/ngx-md-email-builder";
+import { IPEmail, Structure, TextBlock } from "@wlocalhost/ngx-email-builder";
 
 @Component({
   standalone: true,
   selector: 'wlocalhost-material-demo-app-entry',
   imports: [MaterialEmailBuilderModule],
   template: `
-    <md-email-builder></md-email-builder>`,
+    <md-email-builder [(value)]="email"></md-email-builder>`,
 })
-export default class RemoteEntryComponent {}
+export default class RemoteEntryComponent {
+  email = new IPEmail([
+    new Structure("cols_1", [
+      [
+        new TextBlock().toObject({}, `
+          <h1 style="margin-bottom: 10px">Hello World</h1>
+          <p>This is a simple email template built with Angular Material</p>
+        `.trim()
+        )
+      ]
+    ])
+  ]);
+}
