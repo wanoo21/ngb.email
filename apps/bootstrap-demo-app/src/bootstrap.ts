432fa59f66ff352d5fa606b67f7ddabd802561b6
wanoo21 <ionprodan33@gmail.com>
2023-05-11 07:26:48 +0300

    feat(bootstrap-demo-app): add navigation to demo

--- apps/bootstrap-demo-app/src/bootstrap.ts
+++ apps/bootstrap-demo-app/src/bootstrap.ts
@@ -1,10 +1,7 @@
-import { ViewEncapsulation } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { RemoteEntryModule } from "./app/remote-entry/entry.module";
 
 platformBrowserDynamic()
-  .bootstrapModule(RemoteEntryModule, {
-    defaultEncapsulation: ViewEncapsulation.Emulated
-  })
+  .bootstrapModule(RemoteEntryModule, {})
   .catch((err) => console.error(err));
