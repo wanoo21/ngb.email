f0523af40402ce6e6786f05796df7a95e4a4cf6c
wanoo21 <ionprodan33@gmail.com>
2023-05-11 10:42:19 +0300

    feat(primeng-demo-app): add some styles and a new default structure

--- apps/primeng-demo-app/src/app/app.component.ts
+++ apps/primeng-demo-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'wlocalhost-root',
+  selector: "wlocalhost-root",
   template: `
     <nav class="flex flex-row px-4 py-2 align-items-center bg-black-alpha-90">
       <a class="no-underline text-white-alpha-90" href="https://ngb.email">
@@ -11,7 +11,8 @@ import { Component } from '@angular/core';
       <div class="ml-auto flex flex-row gap-2 align-items-center">
         <ul class="flex flex-row gap-2 list-none">
           <li class="nav-item">
-            <a class="no-underline text-white-alpha-90" href="https://docs.ngb.email/templates/default-templates/primeng-email-builder">
+            <a class="no-underline text-white-alpha-90"
+               href="https://docs.ngb.email/templates/default-templates/primeng-email-builder">
               How To Install
             </a>
           </li>
@@ -19,12 +20,20 @@ import { Component } from '@angular/core';
             <a class="no-underline text-white-alpha-90" href="https://ngb.email">← Back To Product Details</a>
           </li>
         </ul>
-        <a href="https://store.wlocalhost.org/l/ngb-14" class="no-underline text-white-alpha-90 ml-2 px-3 py-2 bg-red-500 border-round hover:bg-red-600">
+        <a href="https://store.wlocalhost.org/l/ngb-14"
+           class="no-underline text-white-alpha-90 ml-2 px-3 py-2 bg-red-500 border-round hover:bg-red-600">
           Get Your License Now 🚀
         </a>
       </div>
     </nav>
     <wlocalhost-primeng-demo-app-entry></wlocalhost-primeng-demo-app-entry>
   `,
+  styles: [`
+      :host {
+          display: block;
+          --ip-email-builder-host-height: calc(100vh - 80px);
+      }`
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+}
