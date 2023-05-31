b8346275f48c9e0c1a640232e07f2c346bffc920
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2023-05-31 16:27:42 +0300

    feat(bulma-email-editor): add hover in structure component

--- libs/bulma-email-builder/src/lib/structure/structure.component.ts
+++ libs/bulma-email-builder/src/lib/structure/structure.component.ts
@@ -1,11 +1,27 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, HostBinding, HostListener } from "@angular/core";
 import { AIPStructure } from "@wlocalhost/ngx-email-builder";
 
 @Component({
-  selector: "bulma-structure",
+  selector: 'bulma-structure',
   templateUrl: 'structure.component.html',
   styleUrls: ['structure.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StructureComponent extends AIPStructure {
+  isHovering = false;
+
+  @HostListener('mouseover')
+  mouseover() {
+    this.isHovering = true;
+  }
+
+  @HostListener('mouseout')
+  mouseout() {
+    this.isHovering = false;
+  }
+
+  @HostBinding('class.hovering')
+  get hovering() {
+    return this.isHovering;
+  }
 }
