dccaee186ad1cd892f129f68d34f9bdf14cabcba
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2023-06-09 18:41:22 +0300

    feat(tailwind-email-editor): disable hover buttons

--- libs/tailwind-email-builder/src/lib/blocks/text-block/text-block.component.ts
+++ libs/tailwind-email-builder/src/lib/blocks/text-block/text-block.component.ts
@@ -15,14 +15,9 @@ export class TextBlockComponent extends TextBlock implements OnInit, OnDestroy {
   editorRight!: Editor;
   override innerText = '';
   toolbar: Toolbar = [
-    ["bold", "italic"],
-    ["underline", "strike"],
-    ["code", "blockquote"],
-    ["ordered_list", "bullet_list"],
     [{ heading: ["h1", "h2", "h3", "h4", "h5", "h6"] }],
     ["link", "image"],
     ["text_color", "background_color"],
-    ["align_left", "align_center", "align_right", "align_justify"]
   ];
 
   form = new FormGroup({
