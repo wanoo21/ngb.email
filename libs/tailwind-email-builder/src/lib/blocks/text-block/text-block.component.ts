bac608785485ee6a13e2aeeda78fba4a2efc70a2
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2023-06-13 13:21:15 +0300

    feat(tailwind-email-editor): add editor changes

--- libs/tailwind-email-builder/src/lib/blocks/text-block/text-block.component.ts
+++ libs/tailwind-email-builder/src/lib/blocks/text-block/text-block.component.ts
@@ -15,9 +15,15 @@
     emailAsideEditor!: Editor;
     override innerText = '';
     toolbar: Toolbar = [
-      [{ heading: ["h1", "h2", "h3", "h4", "h5", "h6"] }],
-      ["link", "image"],
-      ["text_color", "background_color"],
+      ['bold', 'italic'],
+      ['underline', 'strike'],
+      ['code', 'blockquote'],
+      ['ordered_list', 'bullet_list'],
+      [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+      ['link', 'image'],
+      ['text_color', 'background_color'],
+      ['align_left', 'align_center', 'align_right', 'align_justify'],
+      ['horizontal_rule', 'format_clear'],
     ];
 
     form = new FormGroup({
