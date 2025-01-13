8f2a7f65c662a1f7cf6597c5328f968e55305458
wanoo21 <ionprodan33@gmail.com>
2025-01-13 16:04:51 +0200

    chore: Full SSR mode and server output

--- apps/root/server.ts
+++ apps/root/server.ts
@@ -1,36 +1,43 @@
-import { AngularNodeAppEngine, createNodeRequestHandler, writeResponseToNodeResponse } from "@angular/ssr/node";
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse
+} from "@angular/ssr/node";
 import express from "express";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  // const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  // const browserDistFolder = resolve(serverDistFolder, "../browser");
-  // const indexHtml = join(serverDistFolder, "index.server.html");
-
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+  const browserDistFolder = resolve(serverDistFolder, "../browser");
   const appEngine = new AngularNodeAppEngine();
 
-  // server.set("view engine", "html");
-  // server.set("views", browserDistFolder);
-
   // Example Express Rest API endpoints
-  server.use("/api", (req, res) => {
+  server.use("/api/**", (req, res) => {
     res.json({ message: "Your API is working!" });
   });
+
   // Serve static files from /browser
-  // server.get(
-  //   "*.*",
-  //   express.static(browserDistFolder, {
-  //     maxAge: "1y"
-  //   })
-  // );
+  server.get(
+    "*.*",
+    express.static(browserDistFolder, {
+      maxAge: "1y"
+    })
+  );
+
+  server.get("/404", (req, res, next) => {
+    res.send("Express is serving this server only error");
+  });
 
   // All regular routes use the Angular engine
   server.use("*", async (req, res, next) => {
     // const { protocol, originalUrl, baseUrl, headers } = req;
 
     try {
-      const response = await appEngine.handle(req);
+      const response = await appEngine.handle(req, { server: "express" });
       if (response) {
         await writeResponseToNodeResponse(response, res);
       } else {
@@ -44,4 +51,15 @@ export function app(): express.Express {
   return server;
 }
 
-export const reqHandler = createNodeRequestHandler(app());
+const server = app();
+if (isMainModule(import.meta.url)) {
+  const port = process.env["PORT"] || 4000;
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:\${port}`);
+  });
+}
+
+console.warn("Node Express server started");
+
+// This exposes the RequestHandler
+export const reqHandler = createNodeRequestHandler(server);
