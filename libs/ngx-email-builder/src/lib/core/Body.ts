0f62d1839077455f41e91143097fbf1bdcce02c6
wanoo21 <ionprodan33@gmail.com>
2025-01-29 14:44:51 +0200

    refactor(ngx-email-builder)!: Apply the new state on Body, Structure and WithSettings classes
    BREAKING CHANGES: Many old methods/properties might not exist in the abstract classes

--- libs/ngx-email-builder/src/lib/core/Body.ts
+++ libs/ngx-email-builder/src/lib/core/Body.ts
@@ -1,47 +1,75 @@
-import { Directive, HostBinding, HostListener, Input, OnInit, output } from "@angular/core";
-import { BehaviorSubject, filter, takeUntil } from "rxjs";
-import { applyDiff } from "recursive-diff";
+import { computed, Directive, OnInit, signal } from '@angular/core';
 
-import { WithSettings } from "./WithSettings";
-import { IPEmail } from "../body/body";
-import { cloneDeep, createBackground, createPadding, mergeObjects } from "../tools/utils";
-import { TDirection } from "../interfaces";
-import { Structure } from "../structure/structure";
-import { IIPValueChanged } from "./ValueChanged";
+import { WithSettings } from './WithSettings';
+import { createBackground, createPadding } from '../tools/utils';
+import { TDirection } from '../interfaces';
+import { injectIIPEmail } from '../state';
+import { IIPEmail } from '../body/body';
+import { debounceTime } from 'rxjs';
+
+const directionLabels = new Map<TDirection, string>([
+  ['ltr', $localize`:@@direction_ltr:Left to right`],
+  ['rtl', $localize`:@@direction_rtl:Right to left`],
+]);
 
 /**
  * The abstract class for the builder body component.
  * It provides a common interface for the settings component to interact with the builder.
  */
-@Directive()
-export abstract class AIPEmailBody extends WithSettings implements OnInit, IIPValueChanged<IPEmail> {
-  // TODO: Skipped for migration because:
-  //  Your application code writes to the input. This prevents migration.
-  @Input() value!: IPEmail;
-  readonly valueChange = output<IPEmail>();
-  contentPart$ = new BehaviorSubject<null | "templates">(null);
-
-  #directionLabels = new Map<TDirection, string>([
-    ["ltr", $localize`:@@direction_ltr:Left to right`],
-    ["rtl", $localize`:@@direction_rtl:Right to left`]
-  ]);
-
-  /**
-   * Get the available direction keys.
-   *
-   * @returns The available direction keys
-   */
-  get directionKeys(): IterableIterator<TDirection> {
-    return this.#directionLabels.keys();
-  }
+@Directive({
+  host: {
+    '[style]': 'hostStyles()',
+    '[attr.dir]': 'dir()',
+    '(click)': 'edit()',
+  },
+})
+export abstract class AIPEmailBody extends WithSettings implements OnInit {
+  readonly contentPart = signal<null | 'templates'>(null);
+  readonly currentEmail = injectIIPEmail();
+  readonly generalOptions = computed(() => this.currentEmail.value().general);
+  readonly structures = computed(() => this.currentEmail.value().structures);
 
   /**
    * Get the available direction options with label and value.
    *
    * @returns The available direction options
    */
-  get directionOptions(): { label: string, value: TDirection }[] {
-    return [...this.directionKeys].map(dir => ({ label: this.getDirectionLabel(dir), value: dir }));
+  readonly directionOptions = [...directionLabels.keys()].map((dir) => ({
+    label: this.getDirectionLabel(dir),
+    value: dir,
+  }));
+
+  /**
+   * Get the host styles binding.
+   *
+   * @returns The host styles binding
+   */
+  readonly hostStyles = computed(() => {
+    const { padding, background } = this.currentEmail.value().general;
+    return {
+      ...createPadding(padding),
+      background: createBackground(background),
+    };
+  });
+
+  /**
+   * Get the dir attribute binding.
+   *
+   * @returns The dir attribute binding
+   */
+  readonly dir = computed(() => this.currentEmail.value().general.direction);
+
+  ngOnInit() {
+    this.builderUiService.setDefaultSettingsPortal(this.settingsPortal());
+    setTimeout(() => {
+      this.form()?.form.patchValue(this.generalOptions());
+    });
+    this.edit();
+    this.form()
+      ?.valueChanges?.pipe(debounceTime(100), this.takeUntilDestroyed)
+      .subscribe((value) => {
+        this.currentEmail.options(value as IIPEmail['general']);
+      });
   }
 
   /**
@@ -51,81 +79,7 @@ export abstract class AIPEmailBody extends WithSettings implements OnInit, IIPVa
    * @returns The direction label
    */
   getDirectionLabel(dir: TDirection): string {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return this.#directionLabels.get(dir)!;
-  }
-
-  /**
-   * Get the host styles binding.
-   *
-   * @returns The host styles binding
-   */
-  @HostBinding("style")
-  get hostStyles(): Record<string, string | number> {
-    const { padding, background } = this.value.general;
-    return {
-      ...createPadding(padding),
-      background: createBackground(background)
-    };
-  }
-
-  /**
-   * Get the dir attribute binding.
-   *
-   * @returns The dir attribute binding
-   */
-  @HostBinding("attr.dir")
-  get dir(): string {
-    return this.value.general.direction;
-  }
-
-  /**
-   * Handles click event on the host.
-   */
-  @HostListener("click") onClick(): void {
-    this.edit();
-  }
-
-  /**
-   * Delete a structure.
-   *
-   * @param structure The structure to be deleted
-   */
-  deleteStructure(structure: Structure): void {
-    const indexOf = this.value.structures.indexOf(structure);
-    this.value.structures.splice(indexOf, 1);
-  }
-
-  /**
-   * Duplicate a structure.
-   *
-   * @param structure The structure to be duplicated
-   */
-  duplicateStructure(structure: Structure): void {
-    const indexOf = this.value.structures.indexOf(structure);
-    this.value.structures.splice(indexOf, 0, cloneDeep(structure));
-  }
-
-  /**
-   * This class provides an implementation of the OnInit interface.
-   * ngOnInit is called after the directive/component's inputs are initialized and the constructor is finished.
-   * It sets the default settings portal and edit mode, and subscribes to commitPush$ observable to update the component's value based on history changes.
-   * It also provides methods to show/hide template list and change value.
-   */
-  ngOnInit() {
-    this.builderUiService.setDefaultSettingsPortal(this.settingsPortal());
-    this.edit();
-    this.historyService.commitPush$.pipe(
-      filter(({ id }) => {
-        const [type, changeId] = id.split(":");
-        return type === "body" && !changeId;
-      }),
-      takeUntil(this.destroyed)
-    ).subscribe(({ diff }) => {
-      mergeObjects(this.value.general, applyDiff(this.value.general, diff));
-      this.valueChange.emit(this.value);
-      this.changeDetectorRef.markForCheck();
-    });
+    return directionLabels.get(dir) as string;
   }
 
   /**
@@ -136,19 +90,12 @@ export abstract class AIPEmailBody extends WithSettings implements OnInit, IIPVa
    */
   showTemplateList($event: Event): void {
     $event.preventDefault();
-    this.contentPart$.next("templates");
+    this.contentPart.set('templates');
   }
 
-  /**
-   * Changes the value of the component.
-   *
-   * @param template The IPEmail object to be set as the new value.
-   * @returns void.
-   */
-  changeValue(template: IPEmail): void {
-    this.value = template;
-    this.valueChange.emit(template);
-    this.contentPart$.next(null);
+  setNewEmailFromTemplate(template: IIPEmail): void {
+    this.currentEmail.set(template);
+    this.contentPart.set(null);
   }
 
   /**
