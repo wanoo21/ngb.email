8b9a4fb081f2a9f55613ce6cbbc51362d45d0366
wanoo21 <ionprodan33@gmail.com>
2025-02-07 15:09:32 +0200

    refactor(ngx-email-builder)!: remove some obsolete pipes/directives
    Refs: #22

--- libs/ngx-email-builder/src/lib/pipes/apply-middleware.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ApplyMiddlewarePipe } from './apply-middleware.pipe';
-
-describe('ApplyMiddlewarePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ApplyMiddlewarePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- libs/ngx-email-builder/src/lib/pipes/template-thumb-path.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TemplateThumbPathPipe } from './template-thumb-path.pipe';
-
-describe('TemplateThumbPathPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TemplateThumbPathPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
