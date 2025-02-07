ec84bb2adc92dd001ce10db3a9f6f86ed6c1c23b
wanoo21 <ionprodan33@gmail.com>
2025-02-07 15:27:03 +0200

    refactor(ngx-email-builder)!: remove some more directives and spec.ts
    Refs: #22

--- libs/ngx-email-builder/src/lib/directives/abstract-email-builder-drop-list.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractEmailBuilderDropList } from './abstract-email-builder-drop-list';
-
-describe('AbstractEmailBuilderDropList', () => {
-  it('should create an instance', () => {
-    expect(new AbstractEmailBuilderDropList()).toBeTruthy();
-  });
-});
