a4974ad4dbe268027895b7a8ef8aa63620f89545
wanoo21 <ionprodan33@gmail.com>
2025-02-07 14:47:08 +0200

    refactor(ngx-email-builder)!: remove pre-defined abstract classes
    BREAKING CHANGES: all predefined abstract classes aren't available
    
    Refs: #25, #22

--- libs/ngx-email-builder/src/lib/tools/core.ts
+++ libs/ngx-email-builder/src/lib/tools/core.ts
@@ -1,46 +1,20 @@
-import { EnvironmentProviders, makeEnvironmentProviders, Type } from "@angular/core";
-import { IP_EMAIL_BUILDER_BLOCKS, IP_EMAIL_BUILDER_BLOCKS_DATA } from "../private-tokens";
-import { AIPEmailBuilderBlock } from "../core/Block";
+import { EnvironmentProviders, makeEnvironmentProviders } from '@angular/core';
+import {
+  IP_EMAIL_BUILDER_BLOCKS,
+  IP_EMAIL_BUILDER_BLOCKS_DATA,
+} from '../private-tokens';
+import type { TIPInjectedBlock } from '../interfaces';
 
-import { TextBlock } from "../blocks/text-block/text.block";
-import { ImageBlock } from "../blocks/image-block/image.block";
-import { ButtonBlock } from "../blocks/button-block/button.block";
-import { DividerBlock } from "../blocks/divider-block/divider.block";
-import { SocialBlock } from "../blocks/social-block/social.block";
-import { SpacerBlock } from "../blocks/spacer-block/spacer.block";
-import { NavigationBlock } from "../blocks/navigation-block/navigation.block";
-import { HtmlBlock } from "../blocks/html-block/html-block";
-
-export function addNewIPEmailBuilderBlock(block: Type<AIPEmailBuilderBlock>, type: string, title: string): EnvironmentProviders {
-  block.prototype.type = type;
+export function addIPEmailBuilderBlock<T>(
+  title: string,
+  block: TIPInjectedBlock<T>
+): EnvironmentProviders {
   return makeEnvironmentProviders([
     { provide: IP_EMAIL_BUILDER_BLOCKS, useValue: block, multi: true },
     {
       provide: IP_EMAIL_BUILDER_BLOCKS_DATA,
-      useValue: { block, type, title },
-      multi: true
-    }
+      useValue: { type: block.type, title, context: block.context },
+      multi: true,
+    },
   ]);
-}
-
-export function addDefaultBlock(block: Type<TextBlock | ImageBlock | ButtonBlock | DividerBlock | SocialBlock | SpacerBlock | NavigationBlock | HtmlBlock>, title: string) {
-  let type = "";
-  if (block.prototype instanceof TextBlock) {
-    type = "text";
-  } else if (block.prototype instanceof ImageBlock) {
-    type = "image";
-  } else if (block.prototype instanceof ButtonBlock) {
-    type = "button";
-  } else if (block.prototype instanceof DividerBlock) {
-    type = "divider";
-  } else if (block.prototype instanceof SocialBlock) {
-    type = "social";
-  } else if (block.prototype instanceof SpacerBlock) {
-    type = "spacer";
-  } else if (block.prototype instanceof NavigationBlock) {
-    type = "navigation";
-  } else if (block.prototype instanceof HtmlBlock) {
-    type = "html";
-  }
-  return addNewIPEmailBuilderBlock(block, type, title);
 }
