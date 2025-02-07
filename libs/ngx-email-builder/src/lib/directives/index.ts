7b57f1f042f4a74d2af67393ded62867e2793dee
wanoo21 <ionprodan33@gmail.com>
2025-02-07 15:43:29 +0200

    refactor(ngx-email-builder)!: remove some more spec.ts and small restructure
    Refs: #22, #23

---
+++ libs/ngx-email-builder/src/lib/directives/index.ts
@@ -0,0 +1,4 @@
+export * from './email-builder-dynamic.directive';
+export * from './email-builder-settings.directive';
+export * from './email-builder-structures.directive';
+export * from './email-builder-column.directive';
--- libs/ngx-email-builder/src/lib/structure/structure.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Structure } from './structure';
-
-describe('Structure', () => {
-  it('should create an instance', () => {
-    expect(new Structure()).toBeTruthy();
-  });
-});
