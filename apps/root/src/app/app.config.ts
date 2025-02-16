e494b00848ed0ae28ee90feb6bf9d3cdd48c1f71
wanoo21 <ionprodan33@gmail.com>
2025-02-16 09:20:28 +0200

    refactor(root): updated configs

--- apps/root/src/app/app.config.ts
+++ apps/root/src/app/app.config.ts
@@ -1,17 +1,15 @@
-import { ApplicationConfig } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { appRoutes } from "./app.routes";
-import { provideHttpClient, withFetch } from "@angular/common/http";
-import { provideNgxEmailBuilderConfig } from "@wlocalhost/ngx-email-builder";
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideNgxEmailBuilderConfig } from '@wlocalhost/ngx-email-builder';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withFetch()),
     provideRouter(appRoutes),
     provideNgxEmailBuilderConfig({
-      convertorPath: "/api/convert",
-      templatesPath: "/templates",
-      templatesThumbsPath: "/thumbnails",
-    })
-  ]
+      convertorPath: '/api/convert',
+    }),
+  ],
 };
